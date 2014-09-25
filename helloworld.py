82525ab048563cadad850130c8efa3bb9365e0d1
Daisuke Miyakawa <d.miyakawa@gmail.com>
2014-09-25 19:00:24 +0900

    Download JSON without actual data

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,11 @@
 # -*- coding: utf-8 -*-
-from google.appengine.api import users
-from google.appengine.ext import ndb
-
-import jinja2
 import webapp2
-
-from datetime import datetime, timedelta
 from json import dumps
-from xml.sax.saxutils import escape
-
 import logging
-import os
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
-
+from google.appengine.ext import ndb
 
 def course_list_key():
     return ndb.Key('CourseList', 'default_course_list')
-
 
 class Course(ndb.Model):
     date = ndb.DateTimeProperty()
@@ -28,82 +13,19 @@ class Course(ndb.Model):
     teacher = ndb.StringProperty()
     detail = ndb.StringProperty()
 
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
         course_model_list = Course.query(ancestor=course_list_key()).order(Course.date)
-        output = self.request.get('output', 'html')
-        if output == 'html':
-#             course_count = course_model_list.count()
-#             template_values = {'course_count': course_count,
-#                                'course_model_list': course_model_list}
-#             template = JINJA_ENVIRONMENT.get_template('index.html')
-#             self.response.write(template.render(template_values))
+        courses = []  # 空のリスト
+        for course_model in course_model_list:
+            single_course = {'date': str(course_model.date),
+                             'title': course_model.title,
+                             'teacher': course_model.teacher,
+                             'detail': course_model.detail}
+            courses.append(single_course)
+        self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+        self.response.write(dumps({'course': courses}, ensure_ascii=False))
 
-            lst = [u'<html><body>']
-            lst.append(u'<h1>講義数合計: {}</h1>'
-                       .format(course_model_list.count()))
-            lst.append(u'<ul>')
-            for course_model in course_model_list:
-                lst.append(u'<li>{}, {}, {}</li>'
-                           .format(escape(course_model.title),
-                                   escape(course_model.teacher),
-                                   escape(course_model.detail)))
-            lst.append(u'</ul>')
-            lst.append(u'<a href="/create">Create</a>')
-            lst.append(u'</body></html>')
-            self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-            for line in lst: self.response.write(line)
-        else:
-            courses = []  # 空のリスト
-            for course_model in course_model_list:
-                single_course = {'date': str(course_model.date),
-                                 'title': course_model.title,
-                                 'teacher': course_model.teacher,
-                                 'detail': course_model.detail}
-                courses.append(single_course)
-            self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
-            self.response.write(dumps({'course': courses}, ensure_ascii=False))
-
-form_html = u'''\
-<html><body>
-<h1>コース作成画面</h1>
-<form action="/create" method="post">
-講義日: <input type="date" name="date"><br>
-講義名: <input type="text" name="title" size="40"><br>
-講師: <input type="text" name="teacher" size="20"><br>
-詳細: <textarea name="detail" rows="4" cols="50"></textarea><br>
-<input type="submit" value="送信">
-</form>
-</body></html>
-'''
-
-class CreateCourse(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(form_html)
-
-    def post(self):
-        date_str = self.request.get('date')
-        if date_str:
-            date = datetime.strptime(date_str, '%Y-%m-%d')
-        else:
-            date = datetime.today()
-        title = self.request.get('title')
-        teacher = self.request.get('teacher')
-        detail = self.request.get('detail')
-        logging.info(u'Saving "{}", "{}", "{}", "{}"'
-                     .format(date, title, teacher, detail))
-        new_course = Course(parent=course_list_key(),
-                            date=date,
-                            title=title,
-                            teacher=teacher,
-                            detail=detail)
-        new_course.put()
-        self.redirect('/')
-
-        
 application = webapp2.WSGIApplication([
-    ("/create", CreateCourse),
     ("/", MainPage),
 ], debug=True)
